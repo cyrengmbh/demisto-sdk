67fb27f172df7f6efa0c249529cdfd4bb447ae5f
guykeller <g12k34ppp>
2020-11-18 17:00:05 +0200

    trying to find MVB

--- demisto_sdk/__main__.py
+++ demisto_sdk/__main__.py
@@ -778,11 +778,12 @@ def generate_doc(**kwargs):
 @click.option(
     "-o", "--output", help="Output file path, the default is the Tests directory.", default='', required=False)
 def id_set_command(**kwargs):
-    id_set_creator = IDSetCreator(**kwargs)
-    print("\n\n\nbefore create_id_set\n\n\n")
-    id_set_creator.create_id_set()
-    print("\n\n\nafter create_id_set\n\n\n")
-    sys.exit(0)
+    raise Exception('TEST FAIL ￿FAILLLLLLLLL')
+    # id_set_creator = IDSetCreator(**kwargs)
+    # print("\n\n\nbefore create_id_set\n\n\n")
+    # id_set_creator.create_id_set()
+    # print("\n\n\nafter create_id_set\n\n\n")
+    # sys.exit(0)
 
 
 # ====================== update-release-notes =================== #
