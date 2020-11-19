8244e1233ccc3041fbaa3fdeefd07f7b998a3211
guykeller <g12k34ppp>
2020-11-19 15:34:06 +0200

    added some lines

--- demisto_sdk/commands/common/update_id_set.py
+++ demisto_sdk/commands/common/update_id_set.py
@@ -1312,9 +1312,9 @@ def re_create_id_set(id_set_path: Optional[str] = DEFAULT_ID_SET_PATH, objects_t
     if id_set_path:
         with open(id_set_path, 'w+') as id_set_file:
             json.dump(new_ids_dict, id_set_file, indent=4)
-    # exec_time = time.time() - start_time
-    # print_color("Finished the creation of the id_set. Total time: {} seconds".format(exec_time), LOG_COLORS.GREEN)
-    # print(f'\n\n\ntime before finding duplicates: {datetime.now().time()}\n\n\n')
+    exec_time = time.time() - start_time
+    print_color("Finished the creation of the id_set. Total time: {} seconds".format(exec_time), LOG_COLORS.GREEN)
+    print(f'\n\n\ntime before finding duplicates: {datetime.now().time()}\n\n\n')
     # duplicates = find_duplicates(new_ids_dict, print_logs)
     # if any(duplicates) and print_logs:
     #     print(
